3df00185ade563b6971e43baef74a77373529654
Usuario <Usuario@DESKTOP-DB7OKJR.fibertel.com.ar>
2025-06-26 22:15:14 -0300

    entrega final de intento mvc

--- ProyectoMVC/src/main/java/com/mycompany/proyectomvc/factory/DAOFactory.java
+++ ProyectoMVC/src/main/java/com/mycompany/proyectomvc/factory/DAOFactory.java
@@ -1,7 +1,7 @@
 package com.mycompany.proyectomvc.factory;
 
 import com.mycompany.proyectomvc.dao.ProductoDAO;
-import com.mycompany.proyectomvc.dao.ProductoDAOimpl;
+import com.mycompany.proyectomvc.dao.ProductoDAOimpl; 
 
 public class DAOFactory {
     public static ProductoDAO getProductoDAO() {
